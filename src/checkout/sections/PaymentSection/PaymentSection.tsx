d5df8cac4248d7e863ecb3785d5f603693f7e008
Lebyopc <officemailidoflebyopc@gmail.com>
2025-12-17 02:15:32 +0530

    Remove debug component - Razorpay integration complete

--- src/checkout/sections/PaymentSection/PaymentSection.tsx
+++ src/checkout/sections/PaymentSection/PaymentSection.tsx
@@ -1,5 +1,4 @@
 import { PaymentMethods } from "./PaymentMethods";
-import { DebugPayments } from "./DebugPayments";
 import { Divider } from "@/checkout/components/Divider";
 import { Title } from "@/checkout/components/Title";
 
@@ -9,7 +8,6 @@ export const PaymentSection = () => {
 			<Divider />
 			<div className="py-4" data-testid="paymentMethods">
 				<Title>Payment methods</Title>
-				<DebugPayments />
 				<PaymentMethods />
 			</div>
 		</>
